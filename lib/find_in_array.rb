106b67ed137884faa21fffd3f0835a0d7f44eb62
Ronalyssa Loiseau <rloiseau99@yahoo.com>
2020-09-21 00:09:25 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,9 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+  index = 0
+  while index < array.length do
+    if array.include?(string)
+    puts index
+    index += 1
+  end
 end
